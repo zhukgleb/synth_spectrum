d2dfe5d65b4fc9ac5c29ed8d3561d97eafc5f033
zhukelvich gleb <zhuklevich.gleb@gmail.com>
2023-11-27 20:46:20 +0300

    feat: displace demo in sep file

--- velocity.py
+++ velocity.py
@@ -35,7 +35,7 @@ def find_velocity(spectrum: list, template: list, inter: list, mult: int):
 
     template = Spectrum1D(spectral_axis=template_ang*u.AA, flux=template_flux*flux_unit)
     observed = Spectrum1D(spectral_axis=spectrum_ang*u.AA, flux=spectrum_flux*flux_unit, uncertainty=StdDevUncertainty(unc))
-    corr, lag = correlation.template_correlate(observed, template, lag_units=u.one)
+    corr, lag = correlation.template_correlate(observed, template, lag_units=u.one, method="fft")
 
 #    plt.plot(lag*299792458, corr)
 #    plt.xlabel("Correlation speed, m/s")
@@ -63,50 +63,3 @@ def find_velocity(spectrum: list, template: list, inter: list, mult: int):
  #       plt.title('Fit to correlation peak')
  #       plt.show()
     return calculate_velocity, z
-
-
-if __name__ == "__main__":
-    s2 = extract_data("data/NES_model_110000.rgs", text=True)
-    s3 = extract_data("data/NES_model_60000.rgs", text=True)
-    s4 = extract_data("data/NES_model_40000.rgs", text=True)
-    s5 = extract_data("data/NES_model_15000.rgs", text=True)
-    s6 = extract_data("data/NES_model_30000.rgs", text=True)
-    a_template, f_template = extract_data("data/NES_model_110000.rgs", text=True)
-
-    spectrum_arr = [s2, s3, s4, s5, s6]
-    spectrum_names = ["R=110000 inter", "R=60000 inter", "R=40000 inter", "R=15000 inter", "R=30000 inter"]
-    spectrum_names_direct = ["R=110000", "R=60000", "R=40000", "R=15000", "R=30000"]
-
-    
-    total_velocity_data = []
-    for i in range(len(spectrum_arr)):
-        velocity = []
-        z_velocity = []
-        true_velocity = []
-        for j in range(1, 100):
-            print(f"speed is {j} meters")
-            _, spectrum_arr[i][0] = pyasl.dopplerShift(spectrum_arr[i][0], spectrum_arr[i][1], j/1000, edgeHandling="firstlast")
-            mult = 50
-            cv, z = find_velocity(spectrum_arr[i], [a_template, f_template], [5000, 5100], mult)
-            velocity.append(cv)
-            z_velocity.append(z)
-            true_velocity.append(j)
-            _, spectrum_arr[i][0] = pyasl.dopplerShift(spectrum_arr[i][0], spectrum_arr[i][1], -j/1000, edgeHandling="firstlast")
-        velocity_data = [true_velocity, velocity, z_velocity]
-        total_velocity_data.append(velocity_data)
-
-
-
-    # A very bad part. btw -- it's time to get it done
-    import matplotlib.pyplot as plt
-    plt.style.use('seaborn-v0_8-talk')
-    plt.grid()
-    for i in range(len(spectrum_arr)):
-        plt.scatter(total_velocity_data[i][0], total_velocity_data[i][2], label=spectrum_names[i], alpha=0.7)
-        plt.scatter(total_velocity_data[i][0], total_velocity_data[i][1], label=spectrum_names_direct[i], marker="+", alpha=0.9)
-    
-    plt.xlabel("Input speed, m/s")
-    plt.ylabel("Output speed, m/s")
-    plt.legend()
-    plt.plot([1, 100], [1, 100], linestyle='dashed', color="gray", alpha=0.5)
-    plt.show()
