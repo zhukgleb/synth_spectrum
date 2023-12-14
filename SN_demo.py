970fb098d0970f4bb95a6e4a49687300001c5501
zhukelvich gleb <zhuklevich.gleb@gmail.com>
2023-12-14 22:11:18 +0300

    feat: add old graphs

--- SN_demo.py
+++ SN_demo.py
@@ -1,6 +1,3 @@
-# Signal Generation
-# matplotlib inline
-
 from data import extract_data
 from velocity import find_velocity
 import PyAstronomy.pyasl as pyasl
@@ -9,23 +6,15 @@ import matplotlib.pyplot as plt
 from data import test_data
 
 
-#x, y = test_data()
-#snr = 1
-#noise = np.random.normal(loc=1, scale=1/snr, size=y.shape)
-#noisy_signal = y + noise
-#plt.plot(x, noisy_signal)
-#plt.show()
-
-
-s2 = extract_data("data/NES_model_110000.rgs", text=True)
-# s3 = extract_data("data/NES_model_60000.rgs", text=True)
+# s2 = extract_data("data/NES_model_110000.rgs", text=True)
+s3 = extract_data("data/NES_model_60000.rgs", text=True)
 # s4 = extract_data("data/NES_model_40000.rgs", text=True)
 s5 = extract_data("data/NES_model_15000.rgs", text=True)
 a_template, f_template = extract_data("data/NES_model_110000.rgs", text=True)
 
-spectrum_arr = [s2, s5] 
-spectrum_names = ["R=110000 inter", "R=15000 inter"]
-spectrum_names_direct = ["R=110000","R=15000"]
+spectrum_arr = [s3, s5] 
+spectrum_names = ["R=60000 inter", "R=15000 inter"]
+spectrum_names_direct = ["R=60000","R=15000"]
 
 
 total_velocity_data = []
@@ -40,15 +29,18 @@ for i in range(len(spectrum_arr)):
     delta_inter = []
     # Now, make a variance between arrays -- add some noise
     # from SN 1 to 100
-    for j in range(1, 100):
+    for j in range(100, 101):
        print(f"speed is {j} meters")
        _, spectrum_arr[i][0] = pyasl.dopplerShift(spectrum_arr[i][0], spectrum_arr[i][1], 20/1000, edgeHandling="firstlast")
        
        noise_spectrum = np.copy(spectrum_arr[i][1])
        noise = np.random.normal(loc=1, scale=1/j, size=len(spectrum_arr[i][1]))
        noise_spectrum = noise_spectrum + noise
+#       import matplotlib.pyplot as plt
+#       plt.plot(spectrum_arr[i][0], noise_spectrum)
+#       plt.show()
 
-       cv, z = find_velocity([spectrum_arr[i][0], noise_spectrum], [a_template, f_template], [5000, 5100], 40)
+       cv, z = find_velocity([spectrum_arr[i][0], noise_spectrum], [a_template, f_template], [5000, 5100], 50)
        velocity.append(cv)
        z_velocity.append(z)
        SN.append(j)
@@ -56,6 +48,7 @@ for i in range(len(spectrum_arr)):
        delta.append(20 - cv) 
     
        _, spectrum_arr[i][0] = pyasl.dopplerShift(spectrum_arr[i][0], spectrum_arr[i][1], -20/1000, edgeHandling="firstlast")
+       del noise_spectrum
  
 
     velocity_data = [velocity, z_velocity]
@@ -65,13 +58,22 @@ for i in range(len(spectrum_arr)):
     
 
 # A very bad part. btw -- it's time to get it done
-plt.style.use('seaborn-v0_8-talk')
-plt.grid()
-for i in range(len(spectrum_arr)):
-    plt.plot(SN, total_delta[i], label=spectrum_names_direct[i])
+from matplotlib.ticker import MultipleLocator
+import matplotlib.font_manager as fm
+gs_font = fm.FontProperties(
+                fname='/System/Library/Fonts/Supplemental/GillSans.ttc')
 
-plt.xlabel("S/N")
-plt.ylabel("Delta")
+plt.style.use('./old-style.mplstyle')
+# plt.grid()
+WIDTH, HEIGHT, DPI = 700, 500, 100
+fig, ax = plt.subplots(figsize=(WIDTH/DPI, HEIGHT/DPI), dpi=DPI)
+linestyle = ['solid', "dashed"]
+
+for i in range(len(spectrum_arr)):
+    ax.plot(SN, total_delta_inter[i], color="k", linestyle=linestyle[i], label=spectrum_names_direct[i])
+
+plt.xlabel("S/N", fontsize=14)
+plt.ylabel("Delta", fontsize=14)
 # plt.yscale("log")
 plt.legend()
 plt.show()
