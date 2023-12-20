c12874f08a38bcec578a98ec57985cd8669ca12e
zhuklevich gleb <zhuklevich.gleb@gmail.com>
2023-12-20 15:15:45 +0300

    fix: remove systematic error

--- SN_demo.py
+++ SN_demo.py
@@ -21,10 +21,11 @@ total_velocity_data = []
 total_delta = []
 total_delta_inter = []
 
-v = 30000  # in meters
+v = 300 * 1000  # in meters
 dots = 50
 
 for i in range(len(spectrum_arr)):
+    print(spectrum_names[i])
     velocity = []
     z_velocity = []
     SN = []
@@ -32,26 +33,29 @@ for i in range(len(spectrum_arr)):
     delta_inter = []
     # Now, make a variance between arrays -- add some noise
     # from SN 1 to 100
-    for j in range(1, 100):
-       print(f"speed is {j} meters")
-       _, spectrum_arr[i][0] = pyasl.dopplerShift(spectrum_arr[i][0], spectrum_arr[i][1], v/1000, edgeHandling="firstlast")
-       
-       noise_spectrum = np.copy(spectrum_arr[i][1])
-       noise = np.random.normal(loc=1, scale=1/j, size=len(spectrum_arr[i][1]))
+    for j in range(1, 101, 10):
+       print(f"SN is {j}")
+       ang = np.copy(spectrum_arr[i][0])
+       flux = np.copy(spectrum_arr[i][1])
+
+       _, ang = pyasl.dopplerShift(ang, flux, v/1000, edgeHandling="firstlast")
+       noise_spectrum = np.copy(flux)
+       noise = np.random.normal(loc=1, scale=1/j, size=len(flux))
        noise_spectrum = noise_spectrum + noise
 #       import matplotlib.pyplot as plt
 #       plt.plot(spectrum_arr[i][0], noise_spectrum)
 #       plt.show()
 
-       cv, z = find_velocity([spectrum_arr[i][0], noise_spectrum], [a_template, f_template], [5000, 5100], dots)
+       cv, z = find_velocity([ang, noise_spectrum], [a_template, f_template], [5000, 5100], dots)
        velocity.append(cv)
        z_velocity.append(z)
        SN.append(j)
        delta_inter.append(v - z)  # For delta graph
        delta.append(v - cv) 
     
-       _, spectrum_arr[i][0] = pyasl.dopplerShift(spectrum_arr[i][0], spectrum_arr[i][1], -v/1000, edgeHandling="firstlast")
        del noise_spectrum
+       del ang
+       del flux
  
 
     velocity_data = [velocity, z_velocity]
@@ -75,6 +79,7 @@ linestyle = ['solid', "dashed"]
 for i in range(len(spectrum_arr)):
     ax.plot(SN, total_delta_inter[i], color="k", linestyle=linestyle[i], label=spectrum_names_direct[i])
 
+plt.title(f"Delta graph for {v} m/s")
 plt.xlabel("S/N", fontsize=14)
 plt.ylabel("Delta", fontsize=14)
 # plt.yscale("log")
