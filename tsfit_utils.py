a1396510e2cd14559bb36b52003c6b5a0245a3d9
zhuklevich gleb <zhuklevich.gleb@gmail.com>
2024-11-07 14:52:07 +0300

    feat: add synth spectrum extractor

--- tsfit_utils.py
+++ tsfit_utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from typing import List, Union
 import os
 
 
@@ -51,29 +52,39 @@ def clean_pd(
     return output_data
 
 
-def get_spectra(path2output) -> np.ndarray:
+def get_spectra(path2output) -> List[Union[np.ndarray, np.ndarray]]:
     # First things First
     # We have a two types spectra in folder -- one is real, observed.
     # and second (actualy, there is two of them) synthetic
     # synthetic have a two versions -- convoled with some R
     # and raw, without any scrab. with flux (!)
+    # TO DO: rewrite as PATH variables, not str
 
+    raw_synth_path = "dummy path"
+    conv_synth_path = "dummy path"
     files_in_output = os.listdir(path2output)
+
+    # not optimal, but i don't give a fuck
+    # ALSO NOT GOOD
     for file in files_in_output:
         if file.find("convolved") != -1:
-            convolved_spectra_name = file
-        if file.find("convolved") == -1 & file.find("result_spectrum"):
-            synth_spectra_name = file
+            conv_synth_path = file
+        if file.find(".txt.spec") != -1:
+            raw_synth_path = file
 
-    print(synth_spectra_name, convolved_spectra_name)
-    output = np.ndarray([1, 2, 3])
-    return output
+    raw_synth_path = path2output + raw_synth_path
+    conv_synth_path = path2output + conv_synth_path
+
+    raw_synth = np.genfromtxt(raw_synth_path)
+    conv_synth = np.genfromtxt(conv_synth_path)
+    return raw_synth, conv_synth
 
 
 if __name__ == "__main__":
-    data_path = "/home/gamma/TSFitPy/output_files/5100_05_VGFe12/"
-    linemask_path = (
-        "/home/gamma/TSFitPy/input_files/linemask_files/Fe/fe12-lmask_VG_clear.txt"
-    )
+    # data_path = "/home/gamma/TSFitPy/output_files/5100_05_VGFe12/"
+    #     linemask_path = (
+    #         "/home/gamma/TSFitPy/input_files/linemask_files/Fe/fe12-lmask_VG_clear.txt"
+    # )
+    data_path = "/home/lambda/TSFitPy/output_files/Oct-04-2024-00-09-14_0.5542094694329981_LTE_Fe_1D/"
     # clean_linemask(linemask_path, data_path)
     get_spectra(data_path)
