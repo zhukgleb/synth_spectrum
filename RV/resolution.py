a3021f80dfd33e90c8e28421a725e2a1cdb8858b
Zhuklevich Gleb <zhuklevich.gleb@gmail.com>
2025-02-20 19:48:58 +0300

    feat: add knot interpolation

--- RV/resolution.py
+++ RV/resolution.py
@@ -2,9 +2,13 @@ from scipy.interpolate import interp1d
 import numpy as np
 
 
-def increese_resolution(spectrum_data: list, coeff: int, method: str = "quad"):
+def increese_resolution(spectrum_data, coeff: int, method: str = "quad"):
     AA_original = spectrum_data[0]
     flux_original = spectrum_data[1]
+    if type(spectrum_data) is np.ndarray:
+        AA_original = spectrum_data[:, 0]
+        flux_original = spectrum_data[:, 1]
+
     AA = np.linspace(np.min(AA_original), np.max(AA_original), len(AA_original) * coeff)
     if method == "quad":
         interp_flux = interp1d(AA_original, flux_original, kind="quadratic")
@@ -13,3 +17,25 @@ def increese_resolution(spectrum_data: list, coeff: int, method: str = "quad"):
 
     flux = interp_flux(AA)
     return AA, flux
+
+
+def interpolate_in_knots(spectrum, knots):
+    if type(spectrum) is np.ndarray:
+        aa = spectrum[:, 0]
+        flux = spectrum[:, 1]
+    elif type(spectrum) is list:
+        aa = spectrum[0]
+        flux = spectrum[1]
+    else:
+        raise
+    interp_flux = interp1d(aa, flux, kind="quadratic")
+    flux = interp_flux(knots)
+
+    return knots, flux
+
+
+if __name__ == "__main__":
+    spec = np.genfromtxt("/Users/beta/synth_spectrum/iras07430.txt")
+    knots = np.arange(spec[:, 0][0], spec[:, 0][-1], 0.005)
+    aa, flux = interpolate_in_knots(spec, knots)
+    np.savetxt("iras07430knots.txt", np.column_stack((aa, flux)))
