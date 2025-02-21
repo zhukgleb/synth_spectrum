df14ceedefb39257315ca9090719ca96b8601531
Zhuklevich Gleb <zhuklevich.gleb@gmail.com>
2025-02-21 15:06:26 +0300

    feat: add short scipt to wavelenght base

---
+++ prepare2anal.py
@@ -0,0 +1,34 @@
+from RV.bf import load_spectra, interpolate_template, calculate_bf_profile, plot_bf_profile
+import numpy as np
+import matplotlib.pyplot as plt
+import PyAstronomy.pyasl as pyasl
+
+
+def velocity_tilt(template, observed):
+    c = 299792.458  # km/s
+    interp_template = interpolate_template(observed, template)
+    bf_profile = calculate_bf_profile(observed, interp_template, v_grid, c)
+    # plot_bf_profile(v_grid, bf_profile)
+    max_bf_index = np.where(bf_profile == max(bf_profile))
+    velocity = v_grid[max_bf_index][0]
+    print(f"velocity is {velocity}")
+    _, observed[:, 0] = pyasl.dopplerShift(observed[:, 0], observed[:, 1], velocity, edgeHandling="firstlast")
+    return observed, velocity
+
+
+template_path = "/Users/beta/synth_spectrum/data/iraszsynth.txt"
+observed_path = "/Users/beta/synth_spectrum/iras07430.txt"
+wavelength_min, wavelength_max = 4700, 5900
+v_grid = np.linspace(-20, 20, 600)
+segment_width = 8
+template, observed = load_spectra(template_path, observed_path, wavelength_min, wavelength_max)
+_, observed[:, 0] = pyasl.dopplerShift(observed[:, 0], observed[:, 1], 15, edgeHandling="firstlast")
+
+velocity = 100
+while abs(velocity) > 0.01:
+    prev_iter = velocity
+    observed, velocity = velocity_tilt(template, observed)
+    if velocity == prev_iter:
+        break
+
+np.savetxt(observed_path + "_rest", observed)
