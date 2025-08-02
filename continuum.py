ccd8c8a91a17863b069a30204ec020f48009e76b
Zhuklevich Gleb <zhuklevich.gleb@gmail.com>
2025-08-02 12:35:07 +0300

    feat: start work on continuum problem

--- continuum.py
+++ continuum.py
@@ -1,1 +1,14 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+    spectrum = np.genfromtxt("data/0.spec")
+
+    wave = spectrum[:, 0]
+    norm_flux = spectrum[:, 1]
+    unnorm_flux = spectrum[:, 2]
+
+    plt.plot(wave, norm_flux)
+    plt.plot(wave, unnorm_flux)
+    plt.show()
